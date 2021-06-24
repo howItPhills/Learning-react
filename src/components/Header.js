fa9589e43d91f22cca8a03fa89a7b99d707acce3
howItPhills <vlad-man001@mail.ru>
2021-06-24 17:39:35 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+   return (
+      <header className='header'>
+         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fadvocate-logo-4.jpg&f=1&nofb=1" className='logo' />
+         <p>for you to see</p>
+      </header>
+   )
+};
+
+export default Header;
---
+++ src/main copy.js
@@ -0,0 +1,10 @@
+function Main() {
+   return (
+      <div>
+         <h1>Kak pozhivaesh</h1>
+         <p>lorem ipsum</p>
+      </div>
+   )
+}
+
+export default Main;
