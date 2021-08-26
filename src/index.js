bda76fd4b09e1fecf766f61907a2614b92af0b34
howItPhills <vlad-man001@mail.ru>
2021-08-26 16:23:15 +0300

    updated package.json

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import store from './redux/redux-store'
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
    <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
          <Provider store={store}>
             <App />
          </Provider>
-      </BrowserRouter>
+      </HashRouter>
    </React.StrictMode>,
    document.getElementById('root')
 );
